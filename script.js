57fdce1b8a81422a905a49c48defb8c05eb32a8d
Drew Maxwell <ajmaxwell.reviresco@gmail.com>
2021-02-22 13:09:15 +0000

    final project

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+
+// array for the characters
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var characterArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+// empty array
+ 
+var resultArray = [];
+var userArray = [];
+
+  //prompts 
+  var numCharacter = window.prompt ("How many Characters do you want in your password?");
+  if (!numCharacter) {
+    window.alert("You need to add a value!")
+    return generatePassword();
+  }
+  var numbers = window.confirm ("you want numbers in your password?");
+  var uppercases = window.confirm ("you want Uppercases in your password?");
+  var lowercases = window.confirm ("you want lowercases in your password?");
+  var characters = window.confirm ("you want special characters in your password?");
+
+
+//conditions for the arrays
+
+
+if (numbers){
+  resultArray = resultArray.concat(numbArray);
+  
+}
+
+if (uppercases){
+  resultArray = resultArray.concat(uppercaseArray);
+
+}
+
+if (lowercases){
+  resultArray = resultArray.concat(lowercaseArray);
+
+}
+
+if (characters){
+  resultArray = resultArray.concat(characterArray);
+}
+console.log(resultArray)
+
+
+//loop for random selection 
+
+for (var i = 0; i < numCharacter; i++) {
+      
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]); 
+  }
+
+  return userArray.join("") ;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -11,7 +66,9 @@ function writePassword() {
 
   passwordText.value = password;
 
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
